61df5dcb45a717f5598fae7fcbd3378d5812c45c
Leonardo-dev-br <99846550+Leonardo-dev-br@users.noreply.github.com>
2025-05-31 17:24:58 -0300

    feat: Iniciando Arquitetura da API

---
+++ src/main/java/montclio/theGuardiansEye/TheGuardiansEyeApplication.java
@@ -0,0 +1,13 @@
+package montclio.theGuardiansEye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGuardiansEyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGuardiansEyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/montclio/theGuardiansEye/TheGuardiansEyeApplicationTests.java
@@ -0,0 +1,13 @@
+package montclio.theGuardiansEye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGuardiansEyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
